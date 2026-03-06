b1798b46b97544bb587ba8898539213c3b2d860a
krystof holecek <krystof.holece.s@gyarab.cz>
2026-03-06 11:46:50 +0100

    update

--- prj/app/views.py
+++ prj/app/views.py
@@ -5,3 +5,15 @@ def home(request):
 
 def about(request):
     return render(request, 'app/about.html')
+
+def login_view(request):
+    return render(request, 'app/login.html')
+
+def profile(request):
+    return render(request, 'app/profile.html')
+
+def detail(request, pk):
+    return render(request, 'app/detail.html')
+
+def add_review(request, pk):
+    return render(request, 'app/add_review.html')
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from app import views  # Import tvých pohledů z aplikace
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),          # Hlavní stránka (KebabTracker)
-    path('about/', views.about, name='about'),  # Stránka O projektu
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('detail/<int:pk>/', views.detail, name='detail'),
+    path('add-review/<int:pk>/', views.add_review, name='add_review'),
 ]

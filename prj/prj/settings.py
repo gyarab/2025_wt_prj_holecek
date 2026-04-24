4c82107ee038ff8093d2c0f2962eda1af248d7d7
krystof holecek <krystof.holece.s@gyarab.cz>
2026-04-24 12:14:24 +0200

    api

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+FIXTURE_DIRS = [
+    BASE_DIR / 'fixtures',
+]
